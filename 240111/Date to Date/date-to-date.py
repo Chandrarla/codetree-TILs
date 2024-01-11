6731e1405ce976fa485c1fc6d3160ce8b6bf707f
문형근 <81503607+Chandrarla@users.noreply.github.com>
2024-01-11 20:00:38 +0900

    Title: Date to Date, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240111/Date to Date/date-to-date.py
+++ 240111/Date to Date/date-to-date.py
@@ -1,12 +1,12 @@
 a, b, c, d = tuple(map(int, input().split()))
-
+// 1 28 3 24
 num_of_days = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 x, y = 0, 0
 
-for i in range(1, a+1):
+for i in range(a):
     x += num_of_days[i]
 
-for j in range(1, c+1):
+for j in range(c):
     y += num_of_days[j]
 if x==y:
     print(1)
