d45db76e0f5e24d1f3412979c4ee6d4e6679d9ba
문형근 <81503607+Chandrarla@users.noreply.github.com>
2024-01-25 10:28:30 +0900

    Title: 최고의 33위치, Time: 104ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240125/최고의 33위치/best-place-of-33.py
@@ -0,0 +1,23 @@
+n = int(input())
+arr = [list(map(int, input().split())) for _ in range(n)]
+
+
+def get_count(row_s, row_e, col_s, col_e):
+    coin_num = 0
+    for row in range(row_s, row_e + 1):
+        for col in range(col_s, col_e + 1):
+            coin_num += arr[row][col]
+    return coin_num
+
+
+max_coin = 0
+for row in range(n):
+    for col in range(n):
+        if row + 3 >= n or col + 3 >= n:
+            continue
+
+        coin_num = get_count(row, row + 2, col, col + 2)
+
+        max_coin = max(max_coin, coin_num)
+
+print(max_coin)
