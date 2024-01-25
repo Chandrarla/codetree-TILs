ba123c12c5daa3716b863675cc800253d95ae37d
문형근 <81503607+Chandrarla@users.noreply.github.com>
2024-01-25 11:31:06 +0900

    Title: 컨베이어 벨트, Time: 211ms, Memory: 26MB, Status: Wrong Answer - CodeTree

--- 240125/컨베이어 벨트/conveyor-belt.py
+++ 240125/컨베이어 벨트/conveyor-belt.py
@@ -2,19 +2,19 @@ n, t = tuple(map(int, input().split()))
 u = list(map(int, input().split()))
 d = list(map(int, input().split()))
 
-temp = u[n-1]
+temp = u[n - 1]
+for _ in range(t):
+    for i in range(n - 1, 0, -1):
+        u[i] = u[i - 1]
+    u[0] = d[n - 1]
 
-for i in range(n-1, 0, -1):
-    u[i] = u[i-1]
-u[0] = d[n-1]
+    for i in range(n - 1, 0, -1):
+        d[i] = d[i - 1]
+    d[0] = temp
 
-for i in range(n-1, 0, -1):
-    d[i] = d[i-1]
-d[0] = temp
+    for elem in u:
+        print(elem, end=" ")
+    print()
 
-for elem in u:
-    print(elem, end=" ")
-print()
-
-for elem in d:
-    print(elem, end=" ")
+    for elem in d:
+        print(elem, end=" ")
