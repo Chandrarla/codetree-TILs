05611d0a4b3d2275f830d4ff4c3892f5c3058a99
문형근 <81503607+Chandrarla@users.noreply.github.com>
2024-01-30 01:31:56 +0900

    Title: 행복한 수열의 개수, Time: 142ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240130/행복한 수열의 개수/number-of-happy-sequence.py
+++ 240130/행복한 수열의 개수/number-of-happy-sequence.py
@@ -1,30 +1,26 @@
 n, m = tuple(map(int, input().split()))
 arr = [list(map(int, input().split())) for _ in range(n)]
-ans = 0
 
 def count_consecutive(arr, m):
-    global ans
-    n = len(arr)
+    total_count = 0
 
     def count_in_line(line):
-        global ans  # 여기에서도 전역 변수 ans 사용 선언
-        cnt = 1
-        prev = line[0]
-        for i in range(1, n):
-            if line[i] == prev:
+        cnt = 0
+        for i in range(1, len(line)):
+            if line[i] == line[i-1]:
                 cnt += 1
-                if cnt == m:  # m개의 연속된 같은 숫자를 발견하면 ans 증가
-                    ans += 1
+                if cnt == m - 1:
+                    return 1
             else:
-                cnt = 1
-                prev = line[i]
+                cnt = 0
+        return 0
 
     for i in range(n):
-        count_in_line(arr[i])
-
+        total_count += count_in_line(arr[i])
     for j in range(n):
-        count_in_line([arr[i][j] for i in range(n)])
+        total_count += count_in_line([arr[i][j] for i in range(n)])
 
+    return total_count
 
-count_consecutive(arr, m)
+ans = count_consecutive(arr, m)
 print(ans)
