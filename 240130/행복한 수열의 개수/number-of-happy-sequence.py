77c5a9a08b1bd2835078258645d6d318ae0ae3e4
문형근 <81503607+Chandrarla@users.noreply.github.com>
2024-01-30 01:33:06 +0900

    Title: 행복한 수열의 개수, Time: 100ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240130/행복한 수열의 개수/number-of-happy-sequence.py
+++ 240130/행복한 수열의 개수/number-of-happy-sequence.py
@@ -1,26 +1,33 @@
 n, m = tuple(map(int, input().split()))
 arr = [list(map(int, input().split())) for _ in range(n)]
+ans = 0
 
-def count_consecutive(arr, m):
-    total_count = 0
-
-    def count_in_line(line):
-        cnt = 0
-        for i in range(1, len(line)):
-            if line[i] == line[i-1]:
-                cnt += 1
-                if cnt == m - 1:
-                    return 1
-            else:
-                cnt = 0
-        return 0
-
+def check_row(arr):
+    global ans
     for i in range(n):
-        total_count += count_in_line(arr[i])
-    for j in range(n):
-        total_count += count_in_line([arr[i][j] for i in range(n)])
+        cnt = 1
+        for j in range(1, n):
+            if arr[i][j] == arr[i][j-1]:
+                cnt += 1
+                if cnt >= m:
+                    ans += 1
+                    break
+            else:
+                cnt = 1
 
-    return total_count
+def check_column(arr):
+    global ans
+    for k in range(n):
+        cnt = 1
+        for l in range(1, n):
+            if arr[l][k] == arr[l-1][k]:
+                cnt += 1
+                if cnt >= m:
+                    ans += 1
+                    break
+            else:
+                cnt = 1
 
-ans = count_consecutive(arr, m)
+check_row(arr)
+check_column(arr)
 print(ans)
