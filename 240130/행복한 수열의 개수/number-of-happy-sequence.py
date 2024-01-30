6145ccd6e86367cbe15053f170c4f2589bfe8409
문형근 <81503607+Chandrarla@users.noreply.github.com>
2024-01-30 14:04:27 +0900

    Title: 행복한 수열의 개수, Time: 124ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240130/행복한 수열의 개수/number-of-happy-sequence.py
+++ 240130/행복한 수열의 개수/number-of-happy-sequence.py
@@ -1,33 +1,41 @@
 n, m = tuple(map(int, input().split()))
+
 arr = [list(map(int, input().split())) for _ in range(n)]
+
 ans = 0
 
-def check_row(arr):
+
+def check_arr(arr):
+    cnt = 1
+    prev = 0
     global ans
+
     for i in range(n):
-        cnt = 1
-        for j in range(1, n):
-            if arr[i][j] == arr[i][j-1]:
-                cnt += 1
-                if cnt >= m:
-                    ans += 1
-                    break
-            else:
+        for j in range(n):
+            if arr[i][j] != prev:
                 cnt = 1
+                prev = arr[i][j]
+            else:
+                cnt += 1
+                prev = arr[i][j]
+        if cnt >= m:
+            ans += 1
 
-def check_column(arr):
-    global ans
+    prev = 0
+
     for k in range(n):
-        cnt = 1
-        for l in range(1, n):
-            if arr[l][k] == arr[l-1][k]:
-                cnt += 1
-                if cnt >= m:
-                    ans += 1
-                    break
-            else:
+        for l in range(n):
+            if arr[l][k] != prev:
                 cnt = 1
+                prev = arr[l][k]
+            else:
+                cnt += 1
+                prev = arr[l][k]
+        if cnt >= m:
+            ans += 1
 
-check_row(arr)
-check_column(arr)
+    return
+
+
+check_arr(arr)
 print(ans)
